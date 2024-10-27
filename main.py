4af4e2a870124a133999902d44e44c130138f92e
Hayyan67 <humairabaig81@gmail.com>
2024-10-27 14:54:36 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+class vehicle:
+    def __int__(self,speed,mileage):
+        self.speed=speed
+        self.mileage=mileage
+        print('Milage:',self.mileage)
+        print('Speed:',self.speed)
+
+v1=vehicle(200,30)
+v2=vehicle(300,32)
